27cf3ea4a99698834d2cece12e03d7af12900869
suresh padavala <Sureshp8374@gmail.com>
2022-08-04 11:58:29 +0530

    Commented the all files by suresh

---
+++ MVC CRUD Operations/Controllers/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿using MVC_CRUD_Operations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CRUD_Operations.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeContext _employeeContext = new EmployeeContext();
+        public ActionResult GetEmployeeDetails(long Id)
+        {
+          var AD=  _employeeContext.Employees.Where(s => s.EmployeeId == Id).FirstOrDefault();
+            return View();
+        }
+
+
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
